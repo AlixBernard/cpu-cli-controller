24166eeeec6dbf1b7eafd115ceb873c4fee5bc08
Alix Bernard <alix.bernard9@gmail.com>
2024-03-27 16:26:29 +0700

    refactor: make `core_num` immutable

--- src/commands.rs
+++ src/commands.rs
@@ -7,8 +7,17 @@ use crate::{CoresArgs, OptionalCoresArgs};
 
 pub fn activate_cmd(args: &OptionalCoresArgs) {
     let all_core_nums = get_all_core_nums();
-    let mut core_nums = match &args.cores {
-        Some(ranges) => get_nums_from_ranges(ranges),
+    let core_nums = match &args.cores {
+        Some(ranges) => {
+            let mut core_nums = get_nums_from_ranges(ranges);
+            if args.no_duplicates {
+                core_nums = core_nums.into_iter().unique().collect();
+            }
+            if args.sort {
+                core_nums.sort()
+            }
+            core_nums
+        }
         None => all_core_nums.clone(),
     };
     if !core_nums.iter().all(|num| all_core_nums.contains(num)) {
@@ -16,39 +25,21 @@ pub fn activate_cmd(args: &OptionalCoresArgs) {
             "Error: the cores selected must be among {:?}",
             all_core_nums
         );
-    }
-    if args.no_duplicates {
-        core_nums = core_nums.into_iter().unique().collect();
-    }
-    if args.sort {
-        core_nums.sort()
     }
     activate_cores(&core_nums);
 }
 
 pub fn deactivate_cmd(args: &CoresArgs) {
     let all_core_nums = get_all_core_nums();
-    let mut core_nums = get_nums_from_ranges(&args.cores);
-    if !core_nums.iter().all(|num| all_core_nums.contains(num)) {
-        panic!(
-            "Error: the cores selected must be among {:?}",
-            all_core_nums
-        );
-    }
-    if args.no_duplicates {
-        core_nums = core_nums.into_iter().unique().collect();
-    }
-    if args.sort {
-        core_nums.sort()
-    }
-    deactivate_cores(&core_nums);
-}
-
-pub fn show_cmd(args: &OptionalCoresArgs) {
-    let all_core_nums = get_all_core_nums();
-    let mut core_nums = match &args.cores {
-        Some(ranges) => get_nums_from_ranges(ranges),
-        None => all_core_nums.clone(),
+    let core_nums = {
+        let mut core_nums = get_nums_from_ranges(&args.cores);
+        if args.no_duplicates {
+            core_nums = core_nums.into_iter().unique().collect();
+        }
+        if args.sort {
+            core_nums.sort()
+        }
+        core_nums
     };
     if !core_nums.iter().all(|num| all_core_nums.contains(num)) {
         panic!(
@@ -56,11 +47,29 @@ pub fn show_cmd(args: &OptionalCoresArgs) {
             all_core_nums
         );
     }
-    if args.no_duplicates {
-        core_nums = core_nums.into_iter().unique().collect();
-    }
-    if args.sort {
-        core_nums.sort()
+    deactivate_cores(&core_nums);
+}
+
+pub fn show_cmd(args: &OptionalCoresArgs) {
+    let all_core_nums = get_all_core_nums();
+    let core_nums = match &args.cores {
+        Some(ranges) => {
+            let mut core_nums = get_nums_from_ranges(ranges);
+            if args.no_duplicates {
+                core_nums = core_nums.into_iter().unique().collect();
+            }
+            if args.sort {
+                core_nums.sort()
+            }
+            core_nums
+        }
+        None => all_core_nums.clone(),
+    };
+    if !core_nums.iter().all(|num| all_core_nums.contains(num)) {
+        panic!(
+            "Error: the cores selected must be among {:?}",
+            all_core_nums
+        );
     }
     show_cores(&core_nums);
 }
